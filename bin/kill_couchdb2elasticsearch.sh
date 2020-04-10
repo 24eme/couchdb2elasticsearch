865aa5f483c444e5c59d4a6f3d449768d361f1ea
Tangui Morlier <tangui@tangui.eu.org>
2020-04-10 13:55:38 +0200

    Mauvais champs

--- bin/kill_couchdb2elasticsearch.sh
+++ bin/kill_couchdb2elasticsearch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ps aux | grep couchdb2elasticsearch.php | grep -v sh  | grep -v grep | awk '{print $5}' | while read pid ; do
+ps aux | grep couchdb2elasticsearch.php | grep -v sh  | grep -v grep | awk '{print $2}' | while read pid ; do
 	kill $pid;
 done
 
