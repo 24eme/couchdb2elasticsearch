2795453e82d0377fcc4736cdcedf8d7a3c195880
Tangui Morlier <tangui@tangui.eu.org>
2020-04-10 14:46:49 +0200

    Info sur le statut de l'indexation

--- bin/index_progress.sh
+++ bin/index_progress.sh
@@ -3,7 +3,17 @@
 cd $(dirname $0)"/.." > /dev/null
 
 ls seqs/* | while read seq_file ; do
-	couchdb=$(grep couchdb $(grep -l $seq_file config*) | sed 's/.*= "//' | sed 's/".*//' )
+	config_file=$(grep -l $seq_file config*)
+	couchdb=$(grep couchdb $config_file | sed 's/.*= "//' | sed 's/".*//' )
 	seq_couchdb=$(curl -s $couchdb | sed 's/.*update_seq"://' | sed 's/["}].*//')
-	echo $seq_file" : "$(echo $( cat $seq_file )" * 100 / "$seq_couchdb | bc)%
+	echo -n $seq_file" : "$(echo $( cat $seq_file )" * 100 / "$seq_couchdb | bc)"% "
+	if test -f "/tmp/couchdb2elasticsearch_"$config_file".lock" ; then
+		if ps aux | grep $config_file | grep -v "sh " | grep -v grep > /dev/null ; then
+			echo "(running)";
+		else
+			echo "(not running but locked)";
+		fi
+	else
+		echo "(not running)";
+	fi
 done
