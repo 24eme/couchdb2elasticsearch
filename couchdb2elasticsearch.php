906c1eff1ba992b105edec04455bb55c8ad2c4c7
Tangui Morlier <tangui@tangui.eu.org>
2020-04-14 18:14:07 +0200

    Si \ dans l'id, c'est qu'il y a une erreur et on passe

--- couchdb2elasticsearch.php
+++ couchdb2elasticsearch.php
@@ -200,6 +200,9 @@ function emit($id, $object, $type, $origin = null) {
 
 function deleteIndexer($change) {
     global $elastic_url_db, $elastic_buffer, $verbose, $lock_file_path;
+    if (preg_match('/\\/', $change->id)) {
+        return;
+    }
     if ($verbose) echo "deleteIndexer (1) : ".$change->id."\n";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $elastic_url_db."/_search?q=source:".urlencode($change->id));
