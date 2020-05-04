5f72396a5ec9502ef9c75b0e8a542967a8133fbb
Tangui Morlier <tangui@tangui.eu.org>
2020-05-04 19:36:14 +0200

    Oups, extra 0

--- bin/index_progress.sh
+++ bin/index_progress.sh
@@ -6,7 +6,7 @@ ls seqs/* | while read seq_file ; do
 	config_file=$(grep -l $seq_file config*)
 	couchdb=$(grep couchdb $config_file | sed 's/.*= *"//' | sed 's/".*//' )
 	seq_couchdb=$(curl -s $couchdb | sed 's/.*update_seq":"*//' | sed 's/["}].*//' | sed 's/-.*//' | sed 's/{/10000000000/')
-	echo -n $seq_file" : "$(echo "*"$( cat $seq_file | sed 's/-.*//' )"0 * 100 / "$seq_couchdb | bc)"% "
+	echo -n $seq_file" : "$(echo "0"$( cat $seq_file | sed 's/-.*/ /')" * 100 / "$seq_couchdb | bc)"% "
 	if test -f "/tmp/couchdb2elasticsearch_"$config_file".lock" ; then
 		if ps aux | grep $config_file | grep -v "sh " | grep -v grep > /dev/null ; then
 			echo "(running)";
