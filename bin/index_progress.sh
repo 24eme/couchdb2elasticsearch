fa7980e97333d5d94fb219b1df4be4d58296d811
Tangui Morlier <tangui@tangui.eu.org>
2020-05-06 10:48:48 +0200

    Ajout debug

--- bin/index_progress.sh
+++ bin/index_progress.sh
@@ -2,11 +2,16 @@
 
 cd $(dirname $0)"/.." > /dev/null
 
+DEBUG=$1;
+
 ls seqs/* | while read seq_file ; do
 	config_file=$(grep -l $seq_file config*)
 	couchdb=$(grep couchdb $config_file | sed 's/.*= *"//' | sed 's/".*//' )
 	seq_couchdb=$(curl -s $couchdb | sed 's/.*update_seq":"*//' | sed 's/["}].*//' | sed 's/-.*//' | sed 's/{/10000000000/')
 	echo -n $seq_file" : "$(echo "0"$( cat $seq_file | sed 's/-.*/ /')" * 100 / "$seq_couchdb | bc)"% "
+	if test "$DEBUG"; then
+  		echo -n " [ "$( cat $seq_file )" / "$seq_couchdb" ] ";
+	fi
 	if test -f "/tmp/couchdb2elasticsearch_"$config_file".lock" ; then
 		if ps aux | grep $config_file | grep -v "sh " | grep -v grep > /dev/null ; then
 			echo "(running)";
