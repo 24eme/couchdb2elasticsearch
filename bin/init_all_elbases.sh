11947f7c7b8653bbbf22df920b0b8fbb1823a610
Gabriel Poma <gpoma@24eme.fr>
2025-03-18 18:43:21 +0100

    fix shellcheck warnings

--- bin/init_all_elbases.sh
+++ bin/init_all_elbases.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $0)/..
 
-ls config*php | while read config ; do 
+find config*php | while read -r config ; do
     curl -X PUT -d @/home/actualys/couchdb2elasticsearch/giilda_mapping.json "$( grep elastic_url_db "$config" | sed 's/'\''/"/g' | awk -F '"' '{print $2}' )"
 done
