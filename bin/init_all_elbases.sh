996006f03fc2e842be5caedfb5d335b0e8ad4f42
Gabriel Poma <gpoma@24eme.fr>
2025-03-18 18:41:37 +0100

    fix syntax error + double quotes

--- bin/init_all_elbases.sh
+++ bin/init_all_elbases.sh
@@ -3,5 +3,5 @@
 cd $(dirname $0)/..
 
 ls config*php | while read config ; do 
-	curl -X PUT -d @/home/actualys/couchdb2elasticsearch/giilda_mapping.json $( grep elastic_url_db $config | sed 's/\'/"/g' | awk -F '"' '{print $2}' ) ; 
+    curl -X PUT -d @/home/actualys/couchdb2elasticsearch/giilda_mapping.json "$( grep elastic_url_db "$config" | sed 's/'\''/"/g' | awk -F '"' '{print $2}' )"
 done
